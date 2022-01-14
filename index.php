4852a94645b1b49a3f744cad91cbb3fa21919488
ProjectWeater <97742494+u2t-saopao@users.noreply.github.com>
2022-01-15 04:47:08 +0700

    Update index.php

--- index.php
+++ index.php
@@ -20,24 +20,27 @@
     if($recv_msg == "Hi") {
 		$rep_msg ['text'] = "Hi";
 		$rep_msg ['type'] = 'text';
-    }else if($recv_msg == "l1") {
-        $url = "https://api.thingspeak.com/channels/1632897/feeds.json?results=1";
-        $strRet = file_get_contents($url);
-		//$strRet = json_decode($strRet);
-		//$light1 = $strRet->feeds[0]->field1;
-		$rep_msg['text'] = $strRet;
-		$rep_msg['type']='text';
-	}else if($recv_msg == "light1 OFF") {
-		$strRet = file_get_contents($url);
-		$strRet = json_decode($strRet);
-		$light1 = $strRet->feeds[0]->field1;
-            if($light1 == "1"){
-                $l1 = "https://api.thingspeak.com/update?api_key=ZRZROJRHC73CR4LJ&field1=0";
-            }else ($light1 == "0"){
-                $l1 = "https://api.thingspeak.com/update?api_key=ZRZROJRHC73CR4LJ&field1=0";
-            }
-		$rep_msg['text'] = "light1 OFF";
-		$rep_msg['type']='text';}
+    }else 
+        $rep_msg ['text'] = "Sorry";
+		$rep_msg ['type'] = 'text';
+    // }else if($recv_msg == "l1") {
+    //     $url = "https://api.thingspeak.com/channels/1632897/feeds.json?results=1";
+    //     $strRet = file_get_contents($url);
+	// 	//$strRet = json_decode($strRet);
+	// 	//$light1 = $strRet->feeds[0]->field1;
+	// 	$rep_msg['text'] = $strRet;
+	// 	$rep_msg['type']='text';
+	// }else if($recv_msg == "light1 OFF") {
+	// 	$strRet = file_get_contents($url);
+	// 	$strRet = json_decode($strRet);
+	// 	$light1 = $strRet->feeds[0]->field1;
+    //         if($light1 == "1"){
+    //             $l1 = "https://api.thingspeak.com/update?api_key=ZRZROJRHC73CR4LJ&field1=0";
+    //         }else ($light1 == "0"){
+    //             $l1 = "https://api.thingspeak.com/update?api_key=ZRZROJRHC73CR4LJ&field1=0";
+    //         }
+	// 	$rep_msg['text'] = "light1 OFF";
+	// 	$rep_msg['type']='text';}
 	// }else if($recv_msg == "light2 ON") {
 	// 	$url = "https://api.thingspeak.com/update?api_key=ZRZROJRHC73CR4LJ&field2=1";
 	// 	$strRet = file_get_contents($url);
