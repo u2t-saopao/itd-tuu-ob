5b2e214e81d50b3954b867dfc2cd46fc4dcb6881
ProjectWeater <97742494+u2t-saopao@users.noreply.github.com>
2022-01-15 03:33:55 +0700

    Create index.php

---
+++ index.php
@@ -0,0 +1,112 @@
+<?php 
+	/*Get Data From POST Http Request*/
+	$datas = file_get_contents('php://input');
+	/*Decode Json From LINE Data Body*/
+	$deCode = json_decode($datas,true);
+
+
+	file_put_contents('log.txt', file_get_contents('php://input') . PHP_EOL, FILE_APPEND);
+
+	$replyToken = $deCode['events'][0]['replyToken'];
+	$recv_msg = $deCode['events'][0]['message']['text'];
+
+
+
+	$messages = [];
+	$messages['replyToken'] = $replyToken;
+	$rep_msg = [];
+
+    if($recv_msg == "light1 ON") {
+		$url = "https://api.thingspeak.com/update?api_key=ZRZROJRHC73CR4LJ&field1=1";
+		$strRet = file_get_contents($url);
+		$strRet = json_decode($strRet);
+		$temp = $strRet->feeds[0]->field1;
+		$rep_msg['text'] = $temp;
+		$rep_msg['type']='text';
+	}else if($recv_msg == "light1 OFF") {
+		$url = "https://api.thingspeak.com/update?api_key=ZRZROJRHC73CR4LJ&field1=0";
+		$strRet = file_get_contents($url);
+		$strRet = json_decode($strRet);
+		$rain = $strRet->feeds[0]->field2;
+		$rep_msg['text'] = $rain;
+		$rep_msg['type']='text';
+	}else if($recv_msg == "light2 ON") {
+		$url = "https://api.thingspeak.com/update?api_key=ZRZROJRHC73CR4LJ&field2=1";
+		$strRet = file_get_contents($url);
+		$strRet = json_decode($strRet);
+		$pm = $strRet->feeds[0]->field3;
+		$rep_msg['text'] = $pm;
+		$rep_msg['type']='text';
+	}else if($recv_msg == "light2 OFF") {
+		$url = "https://api.thingspeak.com/update?api_key=ZRZROJRHC73CR4LJ&field2=0";
+		$strRet = file_get_contents($url);
+		$strRet = json_decode($strRet);
+		$pict = $strRet->feeds[0]->field4;
+		$rep_msg['text'] = $pict;
+		$rep_msg['type']='text';
+	}else if($recv_msg == "Temp") {
+		$rep_msg['originalContentUrl'] = "https://thingspeak.com/channels/1632897/charts/3";
+		$rep_msg['previewImageUrl'] = "https://i.imgur.com/ObxhSgt.png";
+		$rep_msg['type']='image';
+    }else if($recv_msg == "Electric") {
+		$url = "https://api.thingspeak.com/channels/1555446/feeds.json?results=1";
+		$strRet = file_get_contents($url);
+		$strRet = json_decode($strRet);
+		$dash = $strRet->feeds[0]->field5;
+		$rep_msg['text'] = $dash;
+		$rep_msg['type']='text';
+    }	
+
+	$messages['messages'][0] =  $rep_msg;
+
+	$encodeJson = json_encode($messages);
+
+	$LINEDatas['url'] = "https://api.line.me/v2/bot/message/reply";
+ 	$LINEDatas['token'] = "SX6uCrFx9UWukh6q05gKkN5Zhgr3JkoOGBm/nRl4cRlvubu47OIwYm2DTenGjJg/SJVzy65LGKWdGgxUNeppPtIyoLHcTl07xnCDh/kLRhBobWhVR1h1D8VboLpJY4oyZMmULZc0+/BNtSUdc3WQhQdB04t89/1O/w1cDnyilFU=";
+  	$results = sentMessage($encodeJson,$LINEDatas);
+
+	/*Return HTTP Request 200*/
+	http_response_code(200);
+
+
+	function sentMessage($encodeJson,$datas)
+	{
+		$datasReturn = [];
+		$curl = curl_init();
+		curl_setopt_array($curl, array(
+		  CURLOPT_URL => $datas['url'],
+		  CURLOPT_RETURNTRANSFER => true,
+		  CURLOPT_ENCODING => "",
+		  CURLOPT_MAXREDIRS => 10,
+		  CURLOPT_TIMEOUT => 30,
+		  CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+		  CURLOPT_CUSTOMREQUEST => "POST",
+		  CURLOPT_POSTFIELDS => $encodeJson,
+		  CURLOPT_HTTPHEADER => array(
+		    "authorization: Bearer ".$datas['token'],
+		    "cache-control: no-cache",
+		    "content-type: application/json; charset=UTF-8",
+		  ),
+		));
+
+		$response = curl_exec($curl);
+		$err = curl_error($curl);
+
+		curl_close($curl);
+
+		if ($err) {
+		    $datasReturn['result'] = 'E';
+		    $datasReturn['message'] = $err;
+		} else {
+		    if($response == "{}"){
+			$datasReturn['result'] = 'S';
+			$datasReturn['message'] = 'Success';
+		    }else{
+			$datasReturn['result'] = 'E';
+			$datasReturn['message'] = $response;
+		    }
+		}
+
+		return $datasReturn;
+	}
+?>
